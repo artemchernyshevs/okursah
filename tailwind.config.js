f4d348ac01613cbbbd24d4b1dee590b647fb5da3
Artem Chernyshev <amcleave@mail.ru>
2022-09-18 20:11:04 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "public/*.{html,js}"
+  ],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {},
+  },
+  plugins: [],
+}
