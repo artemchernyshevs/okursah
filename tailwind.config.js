bd0d14630dd70b4a03f67feac12d57a1ba118c36
Artem Chernyshev <amcleave@mail.ru>
2022-11-21 02:14:01 +0300

    style: add fonts, change container width

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -6,8 +7,19 @@ module.exports = {
   theme: {
     container: {
       center: true,
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1280px'
+      }
     },
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
+      }
+    },
   },
   plugins: [],
 }
